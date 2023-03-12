32d8a68d1386b0c5adc15bbfa3b463ea4cb7f33e
Guillaume Fayard <guillaume.fayard@pycolore.fr>
2023-03-12 17:56:28 +0100

    move state

--- app/src/features/OnlineGame/OnlineGame.tsx
+++ app/src/features/OnlineGame/OnlineGame.tsx
@@ -1,8 +1,16 @@
+import { teamsPresenceState } from "db/realtime";
 import FirstPlayerDraw from "features/FirstPlayerDraw/FirstPlayerDraw";
 import Game from "features/game/Game/Game";
 import OnlineLobby from "features/OnlineLobby/OnlineLobby";
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Team, TeamsToDefine } from "types";
+import * as uuid from "uuid";
+
+export interface UserPayload {
+  id: string;
+  name: string;
+  team?: Team;
+}
 
 interface Props {
   id: string;
@@ -10,12 +18,51 @@ interface Props {
 
 function OnlineGame({ id }: Props) {
   const [teamNames, setTeamNames] = useState<TeamsToDefine>();
-  const [onlineTeam, setOnlineTeam] = useState<Team | null>(null);
+  const [onlineTeam, setOnlineTeam] = useState<Team | null>(null); // team of local player
   const [name, setName] = useState<string>();
+  const [users, setUsers] = useState<UserPayload[]>([]);
+  const userId = useMemo(() => uuid.v4(), []);
+  const gameIsOngoing = useMemo(() => {
+    const teams = users.map((user) => user.team);
+    return (
+      teams.every((t) => t === Team.team1) ||
+      teams.every((t) => t === Team.team2)
+    );
+  }, [users]);
+
+  useEffect(() => {
+    if (name === undefined) return;
+    const channel = teamsPresenceState({ gameId: id });
+    channel
+      .on<UserPayload>(
+        "presence",
+        { event: "join" },
+        ({ key, newPresences, currentPresences }) => {
+          if (key !== "presences") return;
+          setUsers([...currentPresences, ...newPresences]);
+        }
+      )
+      .on<UserPayload>(
+        "presence",
+        { event: "leave" },
+        ({ key, currentPresences }) => {
+          if (key !== "presences") return;
+          setUsers(currentPresences);
+        }
+      )
+      .subscribe(async (status) => {
+        if (status === "SUBSCRIBED")
+          await channel.track({ id: userId, name, team: onlineTeam });
+      });
+    return () => {
+      channel.unsubscribe();
+    };
+  }, [name, onlineTeam]);
 
   return (
     <>
       <OnlineLobby
+        users={users}
         gameId={id}
         team={onlineTeam}
         teamNames={teamNames}
--- app/src/features/OnlineLobby/OnlineLobby.tsx
+++ app/src/features/OnlineLobby/OnlineLobby.tsx
@@ -1,8 +1,6 @@
-import { teamsPresenceState } from "db/realtime";
-import { useEffect, useMemo, useState } from "react";
 import EnterName from "./EnterName";
-import * as uuid from "uuid";
 import { Team, TeamsToDefine } from "types";
+import { UserPayload } from "features/OnlineGame/OnlineGame";
 
 interface Props {
   gameId: string;
@@ -11,55 +9,18 @@ interface Props {
   name?: string;
   setName: (name: string) => void;
   teamNames?: TeamsToDefine;
-}
-
-interface UserPayload {
-  id: string;
-  name: string;
-  team?: Team;
+  users: UserPayload[];
 }
 
 function OnlineLobby({
   gameId,
-  team,
+  users,
   setTeam,
   teamNames,
   name,
   setName,
 }: Props) {
-  const userId = useMemo(() => uuid.v4(), []);
-  const [users, setUsers] = useState<UserPayload[]>();
-  const [isReady, setIsReady] = useState(false);
   const gameUrl = `http://localhost:3000/en-ligne/${gameId}`;
-
-  useEffect(() => {
-    if (name === undefined) return;
-    const channel = teamsPresenceState({ gameId });
-    channel
-      .on<UserPayload>(
-        "presence",
-        { event: "join" },
-        ({ key, newPresences, currentPresences }) => {
-          if (key !== "presences") return;
-          setUsers([...currentPresences, ...newPresences]);
-        }
-      )
-      .on<UserPayload>(
-        "presence",
-        { event: "leave" },
-        ({ key, currentPresences }) => {
-          if (key !== "presences") return;
-          setUsers(currentPresences);
-        }
-      )
-      .subscribe(async (status) => {
-        if (status === "SUBSCRIBED")
-          await channel.track({ id: userId, name, team, isReady });
-      });
-    return () => {
-      channel.unsubscribe();
-    };
-  }, [name, team, isReady]);
 
   if (name === undefined) return <EnterName onSubmitName={setName} />;
   return (
@@ -72,26 +33,19 @@ function OnlineLobby({
         )}
       </ul>
       {teamNames?.team1 || "Équipe 1"}
-      <button disabled={isReady} onClick={() => setTeam(Team.team1)}>
-        Rejoindre
-      </button>
+      <button onClick={() => setTeam(Team.team1)}>Rejoindre</button>
       <ul>
         {users?.map(({ id, name, team }) =>
           team === Team.team1 ? <li key={id}>{name}</li> : null
         )}
       </ul>
       {teamNames?.team2 || "Équipe 2"}
-      <button disabled={isReady} onClick={() => setTeam(Team.team2)}>
-        Rejoindre
-      </button>
+      <button onClick={() => setTeam(Team.team2)}>Rejoindre</button>
       <ul>
         {users?.map(({ id, name, team }) =>
           team === Team.team2 ? <li key={id}>{name}</li> : null
         )}
       </ul>
-      <button onClick={() => setIsReady((s) => !s)}>
-        {!isReady ? "Je suis prêt" : "Je ne suis pas prêt"}
-      </button>
       <p>Copiez ce lien pour inviter vos amis dans la partie : {gameUrl}</p>
     </div>
   );
