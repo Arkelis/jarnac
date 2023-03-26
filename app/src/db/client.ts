52dd5a44e8268edc5c66d14afe39b42e4ec50191
Guillaume Fayard <guillaume.fayard@pycolore.fr>
2023-03-26 19:02:22 +0200

    CRA -> Vite

--- app/src/db/client.ts
+++ app/src/db/client.ts
@@ -2,5 +2,5 @@ import { createClient } from "@supabase/supabase-js"
 
 export const supabase = createClient(
   "https://kskatigvwydjkupclwep.supabase.co",
-  process.env.REACT_APP_SUPABASE_API_KEY || ""
+  import.meta.env.VITE_SUPABASE_API_KEY || ""
 )
