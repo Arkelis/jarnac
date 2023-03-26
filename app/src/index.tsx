a928a5011971e3973d44325703dd1a9d63fbc088
Guillaume Fayard <guillaume.fayard@pycolore.fr>
2023-03-26 18:43:17 +0200

    update deps

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
-import reportWebVitals from "./reportWebVitals"
 import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom"
 import StartPage from "pages/StartPage"
 import LocalGamePage from "pages/LocalGamePage"
@@ -24,8 +23,3 @@ root.render(
     </QueryClientProvider>
   </React.StrictMode>
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
